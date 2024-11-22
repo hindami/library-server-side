c4f1afeec6d3f84a6d8d45366f47edc4ca52b9a0
hindami <hindami050294@gmail.com>
2024-11-22 13:44:46 +0700

    feat: db config

---
+++ src/config/db.ts
@@ -0,0 +1,12 @@
+import pg from 'pg';
+import 'dotenv/config';
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+
+export default db;
